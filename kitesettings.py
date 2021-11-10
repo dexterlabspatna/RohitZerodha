f35937abadc3f41cb04abb1f46698dd10444141e
dexterlabspatna <88551522+dexterlabspatna@users.noreply.github.com>
2021-11-10 09:02:26 +0530

    Update kitesettings.py

--- kitesettings.py
+++ kitesettings.py
@@ -2,4 +2,4 @@
 API_KEY = "nsufmijvfkjze0cm"
 api_secret = "ivii1r9i4hs1e3eb71wdo4zhvsymmjwn"
 # ====================================================
-access_token = "tx5La0X67AUEW1XGz73XAEkk5hFguKlh"
+access_token = "L8Bq55oxgEyOL9J9cBGz2tC9yR2ibIl5"
